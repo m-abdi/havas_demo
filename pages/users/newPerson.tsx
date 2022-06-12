61c8e1d7cb272962e2a48675ab76f4d6cb2770c7
armanpoor <105088202+armanpoor@users.noreply.github.com>
2022-06-12 22:37:25 +0430

    new person added

--- pages/users/newPerson.tsx
+++ pages/users/newPerson.tsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function newPerson() {
   return (
-    <div>newPerson</div>
+   <>
+        <Section >
+          <Titr>
+            <ChatIcon/>
+            <Typography sx={{paddingBottom:'5px', paddingRight:'5px'}}>عمومی</Typography>
+          </Titr>
+          <Row1>
+            <Input1>
+              <Label1>عنوان</Label1>
+              <TextField />
+            </Input1>
+            <Input1>
+              <Label1 sx={{marginLeft:'0px !important'}}>مسولیت</Label1>
+              <TextField />
+            </Input1>
+          </Row1>
+          <Row1>
+            <Input1>
+              <Label1>مکان فعالیت</Label1>
+              <TextField />
+            </Input1>
+          </Row1>
+        </Section>
+        <Address/>
+        <Call/>
+      </>
   )
 }
