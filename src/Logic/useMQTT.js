8d1d61bf6585744a85cd6a7ac4653ff12f11222c
Mehdi Abdi <m.abdi.public@gmail.com>
2022-09-16 22:57:38 +0430

    dynamic useSSL

--- src/Logic/useMQTT.js
+++ src/Logic/useMQTT.js
@@ -68,7 +68,7 @@ export default function useMQTT(channel = 'rfid') {
       }
       client.connect({
         onSuccess: onConnect,
-        useSSL:false,
+        useSSL: Boolean(parseInt(process.env.NEXT_PUBLIC_MQTT_BROKER_SSL)),
         userName: process.env.NEXT_PUBLIC_MQTT_BROKER_USERNAME,
         password: process.env.NEXT_PUBLIC_MQTT_BROKER_PASSWORD,
       });
@@ -85,7 +85,7 @@ export default function useMQTT(channel = 'rfid') {
     // connect the client
     client.connect({
       onSuccess: onConnect,
-      useSSL: false,
+      useSSL: Boolean(parseInt(process.env.NEXT_PUBLIC_MQTT_BROKER_SSL)),
       userName: process.env.NEXT_PUBLIC_MQTT_BROKER_USERNAME,
       password: process.env.NEXT_PUBLIC_MQTT_BROKER_PASSWORD,
     });
